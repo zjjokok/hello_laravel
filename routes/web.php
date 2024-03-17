469d83fa62c9b47ca22e6d45f1742aa707829f62
zhaojj <76563485@qq.com>
2024-03-17 21:46:06 +0800

    路由命名

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'StaticPagesController@home');
-Route::get('/about', 'StaticPagesController@about');
-Route::get('/help', 'StaticPagesController@help');
+Route::get('/', 'StaticPagesController@home')->name('home');
+Route::get('/about', 'StaticPagesController@about')->name('about');
+Route::get('/help', 'StaticPagesController@help')->name('help');
